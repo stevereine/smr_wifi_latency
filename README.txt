86f66120d12519498dc9feb119b7aa7495d774bd
steve reine <s.reine@comcast.net>
2021-01-31 17:48:41 -0500

    Based on the first push from source, this is the first commit On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   ESP8266_latency.ino
    	new file:   README.txt
    	new file:   wifi_latency_pyqt4.py
    	new file:   wifi_latency_pyqt5.py

---
+++ README.txt
@@ -0,0 +1,52 @@
+﻿This is my first foray into using the Github DCVS for my project storage. I am an EE by trade, and as
+a hobbyist on the side I toy with network control of sensors and motor control. The work I do is mainly
+with the Sparkfun ESP8266 wifi shield, using the Arduino Uno or Mega.
+
+This project is a simple one, just testing the latency of a socket connection from a client connecting
+to a wifi server set up on the Arduino Uno, using the Sparkfun ESP8266 wifi shield.
+
+There are two files contained in the project. The only difference is that one uses PyQt4, and the other PyQt5.
+
+I have run both of these files successfully on;
+
+Python 2.7
+Python 3.5
+
+and on;
+
+Debian Buster
+RPi
+
+I have tried to run these on the latest Ubuntu, 20.10, and have been successful running wifi_latency_pyqt5.py
+with Python 3. However, Ubuntu does not seem to support PyQt4 any longer, or support PyQt5 in Python 2.7. No big
+deal, it was just an experiment on my part to see how backwards compatible I could make this code.
+
+Running this code, I have found a mean latency of about 1.2 seconds, looks like maybe a one sigma distribution
+of about 1.15 to 1.25s. This is OK for temp sensors and such, but not for motor control. I have other experiments
+in mind using direct radio control for motors. I will post other projects if I am successful.
+
+Note that I use the LiquidCrystal Arduino library, as I have an HD44780 16x2 LCD display mounted on an Arduino
+prototyping board. There are potential conficts between the electrical connections need for the wifi shield and 
+for the LCD display. This is reflected in the arguments that I have used in the;
+
+LiquidCrystal lcd()
+
+function in this code. You will need to wired your board to reflect this function call, or change the function 
+call and your wiring per your own needs.
+
+Although it's not noted in these files, I have set the ESP8266 wifi shield to software control and I am using 
+pin 8 (tx) and pin 9 (rx) for the Software Serial control. You will need to edit your Software Serial library to
+reflect this.
+
+For my own security reason, I left the following arguments in the .ino code empty. You will need to add your own.
+
+const char mySSID[] = "";
+const char myPSK[] = "";
+
+There is a lot of the ESP8266 example code left in the Arduino code. My apologies that I have not cleaned this up.
+
+I'm happy to help, please contact me if you have interest in this, or run into issues making it run.
+
+Steve
+
+s.reine@comcast.net         
---
+++ wifi_latency_pyqt5.py
@@ -0,0 +1,92 @@
+import PyQt5, sys, socket, faulthandler, time
+
+from PyQt5.QtWidgets import *
+
+from PyQt5.QtCore import QTimer
+
+from datetime import datetime
+
+class latency(QWidget):
+    #def datad_function(self, parent=None):
+    #    dw.scratchpad2.setText("datad_function called")    
+    #    mylog()
+            
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        
+        faulthandler.enable()
+        
+        self.setGeometry(200, 200, 400, 300)
+        self.setWindowTitle('Arduino, ESP8266 Latency Test')
+
+        myfont = PyQt5.QtGui.QFont('SansSerif', 13)
+        
+        button=QPushButton('start', self)
+        button.setFont(myfont)
+        button.setGeometry(150, 10, 200, 40)
+        button.clicked.connect(self.time_function)
+        
+        label_start=QLabel('time start', self)
+        label_start.setFont(myfont)
+        label_start.setGeometry(10, 60, 100, 40)
+        
+        label_stop=QLabel('time return', self)
+        label_stop.setFont(myfont)
+        label_stop.setGeometry(10, 110, 100, 40)
+        
+        label_diff=QLabel('interval (ms)', self)
+        label_diff.setFont(myfont)
+        label_diff.setGeometry(10, 160, 100, 40)
+        
+        self.start_field=QLineEdit(self)
+        self.start_field.setFont(myfont)
+        self.start_field.setGeometry(150, 60, 200, 40)
+        self.start_field.setText("time: start")
+        
+        self.stop_field=QLineEdit(self)
+        self.stop_field.setFont(myfont)
+        self.stop_field.setGeometry(150, 110, 200, 40)
+        self.stop_field.setText("time: stop")
+        
+        self.int_field=QLineEdit(self)
+        self.int_field.setFont(myfont)
+        self.int_field.setGeometry(150, 160, 200, 40)
+        self.int_field.setText("time: interval")
+        
+        self.timer = QTimer()
+        self.timer.setInterval(5000)
+        self.timer.timeout.connect(self.time_function)
+        self.timer.start()
+        
+    def time_function(self):
+        tb = time.localtime()
+        time_str = time.strftime("%Y-%m-%d %H-%M-%S", tb)
+        self.start_field.setText(time_str)
+        
+        now_b = datetime.now()
+        
+        HOST='192.168.1.116'
+        PORT=80
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, PORT))#connect to 192.168.1.121, Arduino 2
+        s.sendto("**dump2".encode(), (HOST, PORT))
+        message = s.recv(60)
+        
+        time_return=message.decode('utf-8')
+        s.close()
+    
+        ta = time.localtime()
+        time_str = time.strftime("%Y-%m-%d %H-%M-%S", tb)
+        #time_str=time_return
+        self.stop_field.setText(time_str)
+        
+        now_a = datetime.now()
+        #self.int_field.setText(now_a-now_b)
+        self.int_field.setText("%0.2f" % (now_a-now_b).total_seconds())
+        
+        
+app=QApplication(sys.argv)
+
+lt=latency()
+lt.show()
+sys.exit(app.exec_())
